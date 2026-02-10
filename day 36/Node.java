7d49e1f5dc6ba2ae5ed4df9cf400a9a6f8ff0788
kethiyan <kithiyonkeralmark@gmail.com>
2026-02-10 14:46:50 +0530

    homework

---
+++ day 36/Node.java
@@ -0,0 +1,16 @@
+
+package singly.linked.list;
+
+
+public class Node
+{
+    int data;
+    Node next;
+    
+    Node(int data)
+    {
+        this.data=data;
+        this.next=null;
+    }
+    
+}
