4007b5ed12ca9c047687ea42bf2b01e2a939d6ea
kethiyan <kithiyonkeralmark@gmail.com>
2026-03-24 13:39:34 +0530

    java Fx

---
+++ javaFx/javafxapplication1/LogInController.java
@@ -0,0 +1,19 @@
+
+package javafxapplication1;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.Initializable;
+
+
+public class LogInController implements Initializable {
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        
+    }    
+    
+}
