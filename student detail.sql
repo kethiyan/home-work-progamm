fbfc9932eafa97f68c9fb324e532fe1a7350f0ef
kethiyan <kithiyonkeralmark@gmail.com>
2026-03-31 11:51:34 +0530

    sql

---
+++ student detail.sql
@@ -0,0 +1,9 @@
+
+CREATE TABLE Student_Table(
+stu_id int PRIMARY KEY,stu_name VARCHAR(50),age int,dob date
+
+);
+select*from Student_Table
+
+INSERT INTO Student_Table VALUES (1, 'Arun', 20, '2005-06-10'),
+ (2, 'Priya', 22, '2003-02-15');
